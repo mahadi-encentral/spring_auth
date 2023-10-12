573fab8aa9ec73e6e6f8d63ea9bd1f83636793e1
MAHADI ABUHURAIRA <mamt4real@gmail.com>
2023-10-12 01:24:41 +0100

    Complete auth

--- src/main/java/com/mamt4real/authentication/AuthenticationApplication.java
+++ src/main/java/com/mamt4real/authentication/AuthenticationApplication.java
@@ -1,9 +1,12 @@
 package com.mamt4real.authentication;
 
+import com.mamt4real.authentication.ephyto.config.EphytoProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(EphytoProperties.class)
 public class AuthenticationApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/mamt4real/authentication/auth/AuthenticationRequest.java
+++ src/main/java/com/mamt4real/authentication/auth/AuthenticationRequest.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthenticationRequest {
+
     String email;
     String password;
 }
